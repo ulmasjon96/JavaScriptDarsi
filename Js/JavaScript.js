f0548a72f4a77ce8d302f1ab5a1bc3f69c64ba08
ulmasjon96 <almatov9642@mail.ru>
2025-04-03 10:10:40 +0500

    Dars Jarayonidagi Yozilgan Kodlar

---
+++ Js/JavaScript.js
@@ -0,0 +1,111 @@
+// let x = prompt(`1 - Son Kiriting: `)
+// let y = prompt(`2 - Son Kiriting: `)
+
+// if (x > y) {
+// 	alert(`1 - son katta`)
+// } else if (x < y) {
+// 	alert(`2 - son katta`)
+// } else {
+// 	alert(`Bu sonlar teng`)
+// }
+
+// // Foydalanuvchidan qiymatlarni olish
+// x = float(input("x ning qiymatini kiriting: "))
+// y = float(input("y ning qiymatini kiriting: "))
+// z = float(input("z ning qiymatini kiriting: "))
+
+// // Maksimal va minimal qiymatlarni topish
+// maximum = max(x, y, z)
+// minimum = min(x, y, z)
+
+// // Natijalarni chiqarish
+// print("Maksimal qiymat:", maximum)
+// print("Minimal qiymat:", minimum)
+
+// let x = +prompt(`1 - Son Kiriting: `)
+// let y = +prompt(`2 - Son Kiriting: `)
+// let z = +prompt(`3 - Son Kiriting: `)
+
+// if (isNaN(x) || isNaN(y) || isNaN(z)) {
+// 	alert("Iltimos, to'g'ri raqam kiriting!")
+// } else {
+// 	if (x === y && y === z) {
+// 		alert(`Sonlar teng`)
+// 	} else if (x > y && x > z) {
+// 		alert(`1 - son eng katta`)
+// 	} else if (y > x && y > z) {
+// 		alert(`2 - son eng katta`)
+// 	} else if (z > x && z > y) {
+// 		alert(`3 - son eng katta`)
+// 	} else if (x < y && x < z) {
+// 		alert(`1 - son eng kichik`)
+// 	} else if (y < x && y < z) {
+// 		alert(`2 - son eng kichik`)
+// 	} else if (z < x && z < y) {
+// 		alert(`3 - son eng kichik`)
+// 	} else {
+// 		alert(`Sonlar teng`)
+// 	}
+// }
+
+// let a = +prompt('1-sonni kirit')
+// let b = +prompt('2-sonni kirit')
+// let c = +prompt('3-sonni kirit')
+
+// // a < b < c
+// if (a < b && b < c) {
+// 	alert(`${a} ${b} dan kichik ${b} ${c} dan kichik`)
+// }
+
+// // a > b > c
+// else if (a > b && b > c) {
+// 	alert(`${a} ${b} dan katta ${b} ${c} dan katta`)
+// }
+
+// // a < b > c
+// else if (a < b && b > c) {
+// 	alert(`${a} ${b} dan kichik ${b} ${c} dan katta`)
+// }
+
+// // a > b < c
+// else if (a > b && b < c) {
+// 	alert(`${a} ${b} dan katta ${b} ${c} dan kichik`)
+// }
+
+// // a < b = c
+// else if (a < b && b == c) {
+// 	alert(`${a} ${b} dan kichik ${b} ${c} ga teng`)
+// }
+
+// // a > b = c
+// else if (a > b && b == c) {
+// 	alert(`${a} ${b} dan katta ${b} ${c} ga teng`)
+// } else {
+// 	alert('Xatolik! Siz son kiritishingiz kerak')
+// }
+
+// let a = +prompt('1-sonni kirit')
+// let b = +prompt('2-sonni kirit')
+// let c = +prompt('3-sonni kirit')
+
+// // a < b < c
+// if (a < b && b < c) {
+// 	alert(`${a} < ${b} < ${c}`)
+// }
+
+// // a > b > c
+// else if (a > b && b > c) {
+// 	alert(`${a} > ${b} > ${c}`)
+// }
+
+// // a < b > c
+// else if (a < b && b > c) {
+// 	alert(`${a} < ${b} > ${c}`)
+// }
+
+// // a > b < c
+// else if (a > b && b < c) {
+// 	alert(`${a} > ${b} < ${c}`)
+// } else {
+// 	alert('Xatolik! Siz son kiritishingiz alertga')
+// }
